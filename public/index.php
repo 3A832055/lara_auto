cb9d8e7cdb99391e5d0bc0fc4433336b8e615f5a
蔡羽宣 <3a832055@gm.student.ncut.edu.tw>
2021-10-13 00:12:36 +0800

    practise

--- public/index.php
+++ public/index.php
@@ -3,7 +3,18 @@ require '../vendor/autoload.php';
 use \Demo\HelloWorld as World;
 use Demo\Hello\Lara;
 use Demo\Hello;
-world = new World();
-lara= new Lara();
-vincent= new Hello\Someone('Vincent');
+$world = new World();
+$lara= new Lara();
+$vincent= new Hello\Someone('Vincent');
 
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('../log/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
